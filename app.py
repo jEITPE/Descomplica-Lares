9c5a05edb6868d17c774bbba5aa6173fd6adf83c
jEITPE <joaopedrojpsantosalmeida@gmail.com>
2025-02-06 22:17:06 -0300

    Adicionando Gunicorn para produção

--- app.py
+++ app.py
@@ -890,4 +890,4 @@ def index():
     return "Funcionando 2025!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=5000)
