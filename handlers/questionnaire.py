f22b2b0ca43a2cfc01535e4088271b1f7f7c2aec
jEITPE <joaopedrojpsantosalmeida@gmail.com>
2025-02-09 20:00:09 -0300

    Modificando o Script para teste

--- handlers/questionnaire.py
+++
@@ -1,8 +0,0 @@
-def handle_questionnaire(client_state, message, from_number):
-    stage = client_state.get_stage()
-    
-    if stage == "questionario_reuniao_nome":
-        return handle_name_question(client_state, message, from_number)
-    elif stage == "questionario_reuniao_idade":
-        return handle_age_question(client_state, message, from_number)
-    # ... outros handlers ... 
